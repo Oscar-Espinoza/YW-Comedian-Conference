586cae55e0f49e03ec11e26bbda57c801f9a6f58
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-23 18:36:38 -0300

    Checking the linter errors

--- app.js
+++ app.js
@@ -11,20 +11,90 @@ const comediansList = [
     description: 'His fans call him the goat because he is a legend',
     background: 'Born in ohio, Dave is known all over the world for his comedy',
   },
-]
+  {
+    name: 'Ali Wong',
+    photoUrl: 'Ali_Wong.jpeg',
+    description: 'One of the best comedians in the US',
+    background: 'Ali Wong was born and raised in the US in the state of Texas',
+  },
+  {
+    name: 'Dave Chapelle',
+    photoUrl: 'Dave_Chapelle.jpeg',
+    description: 'His fans call him the goat because he is a legend',
+    background: 'Born in ohio, Dave is known all over the world for his comedy',
+  },
+  {
+    name: 'Ali Wong',
+    photoUrl: 'Ali_Wong.jpeg',
+    description: 'One of the best comedians in the US',
+    background: 'Ali Wong was born and raised in the US in the state of Texas',
+  },
+  {
+    name: 'Dave Chapelle',
+    photoUrl: 'Dave_Chapelle.jpeg',
+    description: 'His fans call him the goat because he is a legend',
+    background: 'Born in ohio, Dave is known all over the world for his comedy',
+  },
+];
 
-const comediansListElement = document.getElementById('comedians-list')
+const comediansListElement = document.getElementById('comedians-list');
+let comediansDisplayed = 0
 
-comediansList.forEach(comedian => {
-  const comedianElement = document.createElement('li')
-  comedianElement.classList.add('comedian')
+for (let i = 0; i < 2; i++) {
+  const comedian = comediansList[i]
+  const comedianElement = document.createElement('li');
+  comedianElement.classList.add('comedian');
   comedianElement.innerHTML = "<div class='photo'><img src='media/" + comedian.photoUrl + "' ></div>" +
   "<div class='information'>" + 
     "<h3 class='name'>" + comedian.name + "</h3>" +
     "<p class='description'>" + comedian.description + "</p>" +
     "<hr>" +
     "<p class='background'>" + comedian.background + "</p>" +
-  "</div>"
+  "</div>";
+  comediansListElement.appendChild(comedianElement);
+  comediansDisplayed++
+}
 
-  comediansListElement.appendChild(comedianElement)
+document.querySelectorAll('.menu-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    if (menu.classList.contains('active')) {
+      menu.classList.remove('active')
+    } else {
+      menu.classList.add('active')
+    }
+  })
 });
+
+document.getElementById('about-link').addEventListener('click', () => {
+  document.getElementById('home-page').classList.remove('active')
+  document.getElementById('about-page').classList.add('active')
+  if (document.getElementById('menu').classList.contains('active')) {
+    document.getElementById('menu').classList.remove('active')
+  }
+})
+
+document.getElementById('nav-logo').addEventListener('click', () => {
+  document.getElementById('about-page').classList.remove('active')
+  document.getElementById('home-page').classList.add('active')
+  if (document.getElementById('menu').classList.contains('active')) {
+    document.getElementById('menu').classList.remove('active')
+  }
+})
+
+document.getElementById('more-btn').addEventListener('click', () => {
+  for (comediansDisplayed; comediansDisplayed < comediansList.length; comediansDisplayed++) {
+    const comedian = comediansList[comediansDisplayed]
+    const comedianElement = document.createElement('li');
+    comedianElement.classList.add('comedian');
+    comedianElement.innerHTML = "<div class='photo'><img src='media/" + comedian.photoUrl + "' ></div>" +
+    "<div class='information'>" + 
+      "<h3 class='name'>" + comedian.name + "</h3>" +
+      "<p class='description'>" + comedian.description + "</p>" +
+      "<hr>" +
+      "<p class='background'>" + comedian.background + "</p>" +
+    "</div>";
+    comediansListElement.appendChild(comedianElement);    
+  }
+  document.getElementById('more-btn').classList.remove('active')
+})
